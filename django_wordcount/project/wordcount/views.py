a2bb3fc9944c95aade03a39398fb75d007b4f206
minjung <minjony1014@gmail.com>
2019-06-04 15:43:22 +0900

    views수정

--- django_wordcount/project/wordcount/views.py
+++ django_wordcount/project/wordcount/views.py
@@ -17,4 +17,4 @@ def result(request) :
         else: 
             word_dictionary[word]=1
 
-    return render(request, 'result.html', {'full':text, 'total':len(words), 'dictionary':word_dictionary.items()})    
+    return render(request, 'result.html', {'full':text, 'total':len(word_dictionary), 'dictionary':word_dictionary.items()})    
